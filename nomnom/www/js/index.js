36a247dfc21fe2ef176e8e0da254b13d6c8c84cc
Daniel <kuroamedeath@hotmail.com>
2015-10-27 23:01:08 -0700

    Somewhat of a general layout
    Doesn't do anything at the moment.

--- nomnom/www/js/index.js
+++ nomnom/www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var controller = null;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,6 +34,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        controller = new Controller();
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
---
+++ nomnom/www/js/restaurantList.js
@@ -0,0 +1,20 @@
+var restaurantList = {
+	//var e = document.getElementById("favouritesList");
+	//var strUser = e.options[e.selectedIndex].text;
+
+	createList: function() {
+
+	}
+
+	deleteList: function() {
+		
+	}
+
+	addRestaurant: function() {
+
+	}
+
+	removeRestaurant: function() {
+		
+	}
+}
